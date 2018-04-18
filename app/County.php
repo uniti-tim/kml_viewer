08c3e02991f236547c2674d14867c60ca570f219
uniti-tim <timothy.carambat@uniti.com>
2018-04-18 13:57:31 -0500

    add county model

---
+++ app/County.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class County extends Model
+{
+  protected $table = 'counties';
+  protected $fillable = ['data'];
+
+  public static function mod_attributes(){
+    #List of attributes of model data record that are able to be edited
+    #Name must match key in DB and type is correct
+    #Available Types: integer,float,string
+    return [
+      'SUB_100'=>"float",
+      'SUB_500'=>"float",
+      'SUB_1000'=>"float",
+      'SUB_2500'=>"float",
+      'SUB_10000'=>"float"
+    ];
+  }
+}
